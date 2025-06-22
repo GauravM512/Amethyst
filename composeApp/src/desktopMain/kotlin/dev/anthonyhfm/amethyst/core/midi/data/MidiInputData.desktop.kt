c70bd01db2a4b3c610c204d1ec2aa2703a0b1c83
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-06-22 16:35:26 +0200

    Noteoff support on Desktop

--- composeApp/src/desktopMain/kotlin/dev/anthonyhfm/amethyst/core/midi/data/MidiInputData.desktop.kt
+++ composeApp/src/desktopMain/kotlin/dev/anthonyhfm/amethyst/core/midi/data/MidiInputData.desktop.kt
@@ -1,10 +1,18 @@
 package dev.anthonyhfm.amethyst.core.midi.data
 
+@OptIn(ExperimentalUnsignedTypes::class)
 actual fun getMidiInputData(byteArray: ByteArray): MidiInputData? {
+    println(byteArray.toUByteArray().contentToString())
+
     if (byteArray.size == 3 && byteArray[0] in 144.toByte() .. 159.toByte()) {
         return MidiInputData(
             pitch = DRUM_RACK_TO_XY[byteArray[1].toInt()],
             velocity = byteArray[2].toInt()
+        )
+    } else if (byteArray.size == 3 && byteArray[0] in 128.toByte() .. 143.toByte()) {
+        return MidiInputData(
+            pitch = DRUM_RACK_TO_XY[byteArray[1].toInt()],
+            velocity = 0
         )
     }
 
