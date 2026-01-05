e851b85cb97c7a056053dd3c1e77d3db8fa84f1c
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2026-01-05 23:44:34 +0100

    MidiPitcher data

--- composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/conversion/ableton/data/devices/MidiPitcher.kt
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/conversion/ableton/data/devices/MidiPitcher.kt
@@ -1,11 +1,18 @@
 package dev.anthonyhfm.amethyst.conversion.ableton.data.devices
 
 import dev.anthonyhfm.amethyst.conversion.ableton.data.AbletonDevice
+import dev.anthonyhfm.amethyst.conversion.ableton.data.utils.AbletonManual
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MidiPitcher(
     @SerialName("Id")
-    val id: Int
-) : AbletonDevice
+    val id: Int,
+    val pitch: Pitch,
+) : AbletonDevice {
+    @Serializable
+    data class Pitch(
+        val manual: AbletonManual<Int>
+    )
+}
