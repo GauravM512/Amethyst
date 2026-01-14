d15aafde1acda9f867368840b84888ec1d2d5650
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2026-01-14 22:33:36 +0100

    iOS Settings in Workspace

--- composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/settings/Settings.kt
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/settings/Settings.kt
@@ -5,7 +5,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,13 +25,22 @@ import dev.anthonyhfm.amethyst.ui.theme.AMETHYST_THEME
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Settings() {
+fun Settings(
+    onBack: (() -> Unit)? = null
+) {
     MaterialTheme(
         colorScheme = AMETHYST_THEME
     ) {
         Scaffold(
             topBar = {
                 TopAppBar(
+                    navigationIcon = {
+                        if (onBack != null) {
+                            IconButton(onClick = onBack) {
+                                Icon(Icons.Default.ArrowBackIosNew, null)
+                            }
+                        }
+                    },
                     title = {
                         Text("Settings")
                     }
--- composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/settings/SettingsDialog.ios.kt
+++ composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/settings/SettingsDialog.ios.kt
@@ -1,7 +1,17 @@
 package dev.anthonyhfm.amethyst.settings
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 actual fun SettingsDialog(visible: Boolean, onDismiss: () -> Unit) {
+    if (visible) Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        Settings(
+            onBack = onDismiss
+        )
+    }
 }
